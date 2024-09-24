a26e8732ef961401dbeb8dfddfe323ff8b83f669
julu <juanluismatalop@gmail.com>
2024-09-24 15:42:45 +0100

    24/09 17:44

--- src/main/java/com/iesvdc/acceso/Borrar.java
+++ src/main/java/com/iesvdc/acceso/Borrar.java
@@ -9,8 +9,13 @@ public class Borrar {
         if (args.length!=4) {
             usage();
         } else {
-            if(args[0].equals("-f") && (args[2].equals("-n")) || args[2].equals("-e")){
+            if(args[0].equals("-f") && (args[1].equals("-n")) || args[1].equals("-e")){
                 // lógica del programa
+                System.out.println("Mandando directorio donde esta el archivo");
+                String ruta = args[0];
+                System.out.println("Obteniendo el archivo a borrar");
+                String archivo = args[1];
+                archivoBorrar(ruta, archivo);
                 //necesitamos buscar los archivos con nombre o extension que queremos eliminar 
                 
 
@@ -25,18 +30,25 @@ public class Borrar {
         System.out.println("java Borrar -f carpeta " +
             "[-n nombre_archivo | -e extension]");
     }
-    private static void archivoBorrar(File archivo){
-        if (archivo.isDirectory()) {
-            File[] files = archivo.listFiles();
-            if (archivo != null) {
+    private static void archivoBorrar(String ruta, String archivo){
+        String[] parts = archivo.split(".");
+        File rutaFile = new File(ruta);
+        File archivoFile = new File(archivo);
+        if (rutaFile.isDirectory()) {
+            if (archivoFile != null) {
+                File[] files = archivoFile.listFiles();
                 for(File file : files){
-                    if(archivo.getName().endsWith(args[2])){
-                        if (archivo.getName().equals(args[2])) {
-                            archivo.delete();
+                    if(archivoFile.getName().endsWith(parts[1])){
+                        if (archivoFile.getName().equals(parts[0])) {
+                            archivoFile.delete();
+                            System.out.println("Archivo borrado con exito");
                         }
                     }
 
                 }
+            }else{
+                System.out.println("No se ha encontrado nada");
             }
+        }
     }
 }
